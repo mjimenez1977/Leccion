7b78f291cf147ef690bb4b1e4218099b873c799c
mjimenez <mjimenez@MR0421010013>
2020-09-08 09:03:53 -0500

    Agregar archivos de proyecto.

---
+++ Leccion/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Leccion.Views;
+using Xamarin.Forms;
+
+
+namespace Leccion
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new PaginaGrid());
+            
+        }
+    }
+}
